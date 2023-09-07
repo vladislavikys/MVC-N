aac08708dd1a1e5c2cf31d6bfcc8e8aa54f8d48a
vladislavikys <zarya.vlad@yandex.ru>
2023-09-08 01:33:39 +0300

    add label text add dataSource and delegate for view

---
+++ MVC-N/Comments/Controller/CommandsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  MVC-N
+//
+//  Created by Влад on 8.09.23.
+//
+
+import UIKit
+
+class CommandsViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+}
+
